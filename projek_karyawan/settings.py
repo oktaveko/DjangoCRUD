156ead6847ed85f874c2bb2d38ffdbae585e2e6a
oktaveko <fransokta77@gmail.com>
2021-11-18 13:23:37 +0700

    update setting for azure

--- projek_karyawan/settings.py
+++ projek_karyawan/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY_KARYAWAN")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -86,10 +86,10 @@ WSGI_APPLICATION = 'projek_karyawan.wsgi.application'
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER' : os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASS"),
-        'HOST' :os.getenv("DB_HOST"),
+        'NAME': os.environ.get("DB_NAME"),
+        'USER' : os.environ.get("DB_USER"),
+        'PASSWORD':os.environ.get("DB_PASS"),
+        'HOST' :os.environ.get("DB_HOST"),
         'PORT' : '3306',
         'OPTIONS' : {
             #'sql_mode' : 'traditional',
